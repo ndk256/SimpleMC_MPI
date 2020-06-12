e01221419a05619bbde9f4fc865c8dba8db01fb4
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-06-12 15:46:40 -0400

    Add MPI initializations and finalizations
    Addition of MPI initialization, creation of 3D grid and associated variables, and MPI_Datatype version of Particle

--- src/main.c
+++ src/main.c
@@ -17,7 +17,26 @@ int main(int argc, char *argv[])
   parse_parameters(parameters);
   read_CLI(argc, argv, parameters);
   print_parameters(parameters);
+  
+  MPI_Init(&argc, &argv);
+  int prcsize, myrank, prcperdim[3], periodicity[3]={0,0,0};
 
+  // Establish MPI values
+  MPI_Comm_size(MPI_COMM_WORLD, &prcsize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm cube; //really ought to have a better name
+  MPI_Dims_create(prcsize, 3, prcperdim);
+  
+  // Define Particle in MPI-sendable format
+  MPI_Datatype PARTICLE;
+  MPI_Datatype base[2] = {MPI_INT, MPI_DOUBLE};
+  int blocks[2] = {2,8};
+  MPI_Aint offsets[2], extent;
+  MPI_Type_extent(MPI_INT, &extent);
+  offsets[0] =0; offsets[1]=blocks[0]*extent;
+  MPI_Type_struct(2, blocks, offsets, base, &PARTICLE);
+  MPI_Type_commit(&PARTICLE);
+  
   // Set initial RNG seed
   set_initial_seed(parameters->seed);
   set_stream(STREAM_INIT);
@@ -27,7 +46,25 @@ int main(int argc, char *argv[])
 
   // Set up geometry
   geometry = init_geometry(parameters);
-
+  if(geometry->bc==PERIODIC){
+    periodicity[0]=1;
+    periodicity[1]=1;
+    periodicity[2]=1;}
+ 
+  // Create 3D topography and orient self
+  MPI_Cart_create(MPI_COMM_WORLD, 3, prcperdim, periodicity, 1, &cube);
+  int mycoords[3];
+  MPI_Cart_coords(cube, myrank, 3, mycoords);
+  double mybounds[6] = {mycoords[0]*(geometry->x/prcperdim[0]), (mycoords[0]+1)*(geometry->x/prcperdim[0]), mycoords[1]*(geometry->y/prcperdim[1]), (mycoords[1]+1)*(geometry->y/prcperdim[1]),mycoords[2]*(geometry->z/prcperdim[2]), (mycoords[2]+1)*geometry->z/prcperdim[2]};
+// ^sorry that's a long line^
+  int myneighb[6];
+  MPI_Cart_shift(cube, 0, -1, &myrank, &myneighb[0]);
+  MPI_Cart_shift(cube, 0, 1, &myrank, &myneighb[1]);
+  MPI_Cart_shift(cube, 1, -1, &myrank, &myneighb[2]);
+  MPI_Cart_shift(cube, 1, 1, &myrank, &myneighb[3]);
+  MPI_Cart_shift(cube, 2, -1, &myrank, &myneighb[4]);
+  MPI_Cart_shift(cube, 2, 1, &myrank, &myneighb[5]);
+  
   // Set up material
   material = init_material(parameters);
 
@@ -65,6 +102,10 @@ int main(int argc, char *argv[])
   free_material(material);
   free(geometry);
   free(parameters);
+  
+  MPI_Type_free(&PARTICLE);
+  MPI_Comm_free(&cube);
+  MPI_Finalize();
 
   return 0;
 }
