82d2a303d2a117a8b21d49ac47a763b21267b872
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-06-12 16:17:21 -0400

    Add localize_parameters()

--- src/localize.c
+++ src/localize.c
@@ -1,5 +1,17 @@
 #include <header.h>
 
+Parameters *localize_parameters(Parameters *par,int dims[])
+   {
+   Parameters *p = malloc(sizeof(Parameters));
+   *p = *par; //i forget how pointers work but hopefully this does
+   
+   p->gx = par->gx/dims[0];
+   p->gy = par->gy/dims[1];
+   p->gz = par->gz/dims[2];
+   return p;
+}
+
+//Sends particles to processes along the axis indicated by dim
 void distrib_particle(int prcoords[], int dim, MPI_Comm comm, Bank *sb, int myrank)
 {
 int d = prcoords[dim]; //number of processes on the given axis
