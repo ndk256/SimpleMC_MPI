d7329af46df563e5bbc3c71c8b2130ec22d0e685
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-06-22 09:58:22 -0400

    Update localize.c
    Now modifies mysb->n

--- src/localize.c
+++ src/localize.c
@@ -68,7 +68,8 @@ if(mycoords[0]!=0)
 {MPI_Probe(MPI_ANY_SOURCE, 0, comm, &status);
 MPI_Get_count(&status, PARTICLE, &msg_size);
 MPI_Recv(mysb->p, msg_size, PARTICLE, MPI_ANY_SOURCE, 0, comm, MPI_STATUS_IGNORE);
-index+=msg_size;}
+index+=msg_size;
+mysb->n += msg_size;}
 
 distrib_particle(prcoords, 1, comm, mysb, myrank);
 }
@@ -82,6 +83,7 @@ MPI_Get_count(&status, PARTICLE, &msg_size);
 MPI_Recv(mysb->p+index, msg_size, PARTICLE, MPI_ANY_SOURCE, 1, comm, &status);
 distrib_particle(prcoords, 2, comm, mysb, myrank);
 index+=msg_size;
+   mysb->n += msg_size;
 }
 
 MPI_Barrier(comm);
@@ -90,7 +92,7 @@ if(mycoords[2]!=0)
 {MPI_Probe(MPI_ANY_SOURCE, 2, comm, &status);
 MPI_Get_count(&status, PARTICLE, &msg_size);
 MPI_Recv(mysb->p+index, msg_size, PARTICLE, status.MPI_SOURCE, 2, comm, &status);
-}
+mysb->n += msg_size;}
 
 mysb->p = realloc(mysb->p, sizeof(Particle)*index); //no idea if this is right
 return;
