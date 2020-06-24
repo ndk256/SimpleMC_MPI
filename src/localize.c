8e6cb6f635d700771bddcd50346f749b6f9ce6fb
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-06-24 07:28:39 -0400

    Update localize.c

--- src/localize.c
+++ src/localize.c
@@ -13,7 +13,7 @@ Parameters *localize_parameters(Parameters *par,int dims[])
 }
 
 //Sends particles to processes along the axis indicated by dim
-void distrib_particle(int prcoords[], int dim, MPI_Comm comm, Bank *sb, int myrank)
+void distrib_particle(int prcoords[], Parameters *p, int dim, MPI_Comm comm, Bank *sb, int myrank)
 {
 int d = prcoords[dim]; //number of processes on the given axis
 int banksz = 0;
@@ -40,14 +40,14 @@ for(int i=1; i<d; i++) // move through the processes in the given dimension from
       }
    }
    MPI_Cart_shift(comm, dim, i, &myrank, &destrank);
-   MPI_Isend(send, banksz, PARTICLE, destrank, dim, comm, MPI_REQUEST_NULL); /// unsure about the last argument
+   MPI_Isend(send, banksz, p->type, destrank, dim, comm, MPI_REQUEST_NULL); /// unsure about the last argument
    banksz=0;
 }   
 
 return;
 }
 
-void distribute_sb(int mycoords[], int prcoords[], int myrank, MPI_Comm comm, Bank *sb, Bank *mysb)
+void distribute_sb(int mycoords[], Parameters *p, int prcoords[], int myrank, MPI_Comm comm, Bank *sb, Bank *mysb)
 {
 MPI_Status status;
 int msg_size, index=0;
@@ -57,7 +57,7 @@ MPI_Barrier(comm);
 //for the process representing (0,0,0)
 if(mycoords[0]==0 && mycoords[1]==0 && mycoords[2] == 0)
 {
-distrib_particle(prcoords, 0, comm, sb, myrank);
+distrib_particle(prcoords, p, 0, comm, sb, myrank);
 }
 
 MPI_Barrier(comm);
@@ -66,12 +66,12 @@ MPI_Barrier(comm);
 if(mycoords[1]==0 && mycoords[2]==0){
 if(mycoords[0]!=0)
 {MPI_Probe(MPI_ANY_SOURCE, 0, comm, &status);
-MPI_Get_count(&status, PARTICLE, &msg_size);
-MPI_Recv(mysb->p, msg_size, PARTICLE, MPI_ANY_SOURCE, 0, comm, MPI_STATUS_IGNORE);
+MPI_Get_count(&status, p->type, &msg_size);
+MPI_Recv(mysb->p, msg_size, p->type, MPI_ANY_SOURCE, 0, comm, MPI_STATUS_IGNORE);
 index+=msg_size;
 mysb->n += msg_size;}
 
-distrib_particle(prcoords, 1, comm, mysb, myrank);
+distrib_particle(prcoords, p, 1, comm, mysb, myrank);
 }
 
 MPI_Barrier(comm);
@@ -79,9 +79,9 @@ MPI_Barrier(comm);
 //for the processes handling (x,y,0)
 if(mycoords[2]==0){
 MPI_Probe(MPI_ANY_SOURCE, 1, comm, &status);
-MPI_Get_count(&status, PARTICLE, &msg_size);
-MPI_Recv(mysb->p+index, msg_size, PARTICLE, MPI_ANY_SOURCE, 1, comm, &status);
-distrib_particle(prcoords, 2, comm, mysb, myrank);
+MPI_Get_count(&status, p->type, &msg_size);
+MPI_Recv(mysb->p+index, msg_size, p->type, MPI_ANY_SOURCE, 1, comm, &status);
+distrib_particle(prcoords, p, 2, comm, mysb, myrank);
 index+=msg_size;
    mysb->n += msg_size;
 }
@@ -90,8 +90,8 @@ MPI_Barrier(comm);
 
 if(mycoords[2]!=0)
 {MPI_Probe(MPI_ANY_SOURCE, 2, comm, &status);
-MPI_Get_count(&status, PARTICLE, &msg_size);
-MPI_Recv(mysb->p+index, msg_size, PARTICLE, status.MPI_SOURCE, 2, comm, &status);
+MPI_Get_count(&status, p->type, &msg_size);
+MPI_Recv(mysb->p+index, msg_size, p->type, status.MPI_SOURCE, 2, comm, &status);
 mysb->n += msg_size;}
 
 mysb->p = realloc(mysb->p, sizeof(Particle)*index); //no idea if this is right
