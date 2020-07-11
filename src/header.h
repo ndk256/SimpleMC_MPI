97bf6fad697773bdbb7748115a58520dd5524ecc
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-07-11 07:52:52 -0400

    Update header.h
    Fixed an outdated declaration of sendrecv_particles()

--- src/header.h
+++ src/header.h
@@ -162,8 +162,8 @@ void free_tally(Tally *tally);
 
 ///localize.c function prototypes
  Parameters *localize_parameters(Parameters *par, int dims[6]);
-void distrib_particle(int prcoords[3], Parameters *p, int dim, MPI_Comm comm, Bank *sb, int myrank);
-void distribute_sb(int mycoords[3], Parameters *p, int prcoords[3], int myrank, MPI_Comm comm, Bank *sb, Bank *mysb); 
+void distrib_particle(int prcoords[3], Parameters *p, int dim, MPI_Comm comm, Bank **sb, int myrank);
+void distribute_sb(int mycoords[3], Parameters *p, int prcoords[3], int myrank, MPI_Comm comm, Bank *sb, Bank **mysb); 
 
 // transport.c function prototypes
 void transport(Parameters *parameters, Geometry *geometry, double local_bounds[], Material *material, Particle tox0[], Particle tox1[], Particle toy0[], Particle toy1[], Particle toz0[], Particle toz1[], int send_indices[], Bank *fission_bank, Tally *tally, Particle *p);
@@ -176,7 +176,7 @@ void collision(Material *material, Bank *fission_bank, double nu, Particle *p);
 
 // eigenvalue.c function prototypes
 void run_eigenvalue(MPI_Comm comm, int myrank, int myneighb[], double localbounds[6], Parameters *parameters, Geometry *geometry, Material *material, Bank *source_bank, Bank *fission_bank, Tally *tally, double *keff);
-void sendrecv_particles(Parameters *p, Bank *bank, Particle sendbank[][6], int send_indices[6], int my_neighb[6], double mybounds[], MPI_Comm comm);
+void sendrecv_particles(Parameters *p, Bank *bank, Particle tox0[], Particle tox1[], Particle toy0[], Particle toy1[], Particle toz0[], Particle toz1[], int send_indices[6], int my_neighb[6], double mybounds[], MPI_Comm comm);
 void synchronize_bank(Bank *source_bank, Bank *fission_bank);
 void calculate_keff(double *keff, double *mean, double *std, int n);
 
