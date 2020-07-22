acecf6afc0475bbd8f09950e656bbcc4969f48e7
ndk256 <64850798+ndk256@users.noreply.github.com>
2020-07-22 15:06:05 -0400

    Update localize.c
    Fixed some if conditions

--- src/localize.c
+++ src/localize.c
@@ -1,33 +1,33 @@
 #include "header.h"
 
-void localize_parameters(Parameters *par,int dims[])
+void localize_parameters(Parameters *par, int dims[])
    {
    par->gx = par->gx/dims[0];
    par->gy = par->gy/dims[1];
    par->gz = par->gz/dims[2];
-   //p->n_bins = par->n_bins/((dims[0]+dims[1]+dims[2])/3); //unsure if needed
-   return par;
+   p->n_bins = par->n_bins/((dims[0]+dims[1]+dims[2])/3); //unsure if needed
+   return;
 }
 
 //Sends particles to processes along the axis indicated by dim
-void distrib_particle(int prcoords[], int mycoords[], Parameters *par, int dim, Bank **sb)
+void distrib_particle(int nprc[], int mycoords[], Parameters *par, int dim, Bank **sb)
 {
-int d = prcoords[dim]; //number of processes on the given axis
+int d = nprc[dim]; //number of processes on the given axis
 int banksz = 0;
-Particle *send; send = (Particle*) malloc((sb->n)*sizeof(Particle)); /// attempting to be dynamic
+Particle *send; send = (Particle*) malloc(((*sb)->n)*sizeof(Particle)); /// attempting to be dynamic
 int destrank;
    
 for(int i=1; i<d; i++) // move through the processes in the given dimension from nearest to farthest
 {
-   for(int i_p=0; i_p<sb->n; i_p++) // iterate through each particle to find if they belong on that process
+   for(int i_p=0; i_p<(*sb)->n; i_p++) // iterate through each particle to find if they belong on that process
    {
-      int pcoords[3] = {(*sb)->p[i_p].x/par->gx, (*sb)->p[i_p].y/par->gy, (*sb)->    p[i_p].z/par->gz};         
+      int pcoords[3] = {(*sb)->p[i_p].x/par->gx, (*sb)->p[i_p].y/par->gy, (*sb)->p[i_p].z/par->gz};         
      //using integer division to get process-level coords
         /// (modify ^ if repeated redeclarations isn't good)
         if(pcoords[dim] != mycoords[dim] && pcoords[dim] == i)
       /// this could be made more efficient; currently it will do a lot of double-checking
       {
-         send[banksz] = sb->p[i_p];
+         send[banksz] = (*sb)->p[i_p];
          banksz++;
          (*sb)->p[i_p].alive = FALSE; ///double-check if correct, but basically kill the version that's not on the right processes
        //experimental alternative:
@@ -38,18 +38,17 @@ for(int i=1; i<d; i++) // move through the processes in the given dimension from
       }
    }
   if(i!=mycoords[dim]){
-   int* temp = malloc(sizeof(int));
-   MPI_Cart_shift(p->comm, dim, i, temp, &destrank);
     MPI_Request mpir = MPI_REQUEST_NULL;
-   MPI_Isend(send, banksz, p->type, destrank, dim, p->comm, &mpir); /// unsure about the last argument
+    MPI_Isend(send, banksz, p->type, p->neighb[2*dim+1], dim, p->comm, &mpir); /// unsure about the last argument
   }
      banksz=0;
 }   
 
+   free(send);
 return;
 }
 
-void distribute_sb(int mycoords[], Parameters *p, int prcoords[], Bank *sb, Bank **mysb)
+void distribute_sb(int mycoords[], Parameters *p, int nprc[], Bank *sb, Bank **mysb)
 {
 MPI_Status status;
 int msg_size, index=0;
@@ -60,15 +59,14 @@ MPI_Barrier(p->comm);
 if(mycoords[0]==0 && mycoords[1]==0 && mycoords[2] == 0)
 {
    (*mysb) = sb;
-distrib_particle(prcoords, mycoords, p, 0, mysb);
+distrib_particle(nprc[0], mycoords, p, 0, mysb);
 }
 
 MPI_Barrier(p->comm);
 
 //for the processes handling (x,0,0)
-if(prcoords[0]>1 && mycoords[1]==0 && mycoords[2]==0){
-if(mycoords[0]!=0)
-{MPI_Probe(MPI_ANY_SOURCE, 0, parameters->comm, &status);
+if(prcoords[0]>1 && mycoords[0]!=0 && mycoords[1]==0 && mycoords[2]==0){
+MPI_Probe(MPI_ANY_SOURCE, 0, parameters->comm, &status);
 MPI_Get_count(&status, p->type, &msg_size);
 MPI_Recv(mysb->p, msg_size, p->type, MPI_ANY_SOURCE, 0, p->comm, MPI_STATUS_IGNORE);
 index+=msg_size;
@@ -76,29 +74,32 @@ index+=msg_size;
    
    for(int i=0; i<index; i++)
       (*mysb)->p[i].alive=TRUE;
-}
-distrib_particle(prcoords, mycoords, p, 1, mysb);
+   
+   if(nprc[1]>1)
+      distrib_particle(nprc[1], mycoords, p, 1, mysb);
 }
 
 MPI_Barrier(p->comm);
 
 //for the processes handling (x,y,0)
-if(prcoords[1] > 1 && mycoords[2]==0 && mycoords[0]!=0) {
+if(prcoords[1] > 1 && mycoords[2]==0 && mycoords[1]!=0) {
 MPI_Probe(MPI_ANY_SOURCE, 1, p->comm, &status);
 MPI_Get_count(&status, p->type, &msg_size);
 MPI_Recv(mysb->p+index, msg_size, p->type, MPI_ANY_SOURCE, 1, p->comm, &status);
    
    for(int i=index; i<index+msg_size; i++)
       (*mysb)->p[i].alive=TRUE;
-   
-distrib_particle(prcoords, mycoords, p, 2, mysb);
-index+=msg_size;
+  
+   if(nprc[2]>1) 
+      distrib_particle(nprc[2], mycoords, p, 2, mysb);
+
+   index+=msg_size;
    (*mysb)->n += msg_size;
 }
 
 MPI_Barrier(p->comm);
 
-if(prcoords[2] > 1 && mycoords[2]!=0)
+if(nprc[2] > 1 && mycoords[2]!=0)
 {MPI_Probe(MPI_ANY_SOURCE, 2, p->comm, &status);
 MPI_Get_count(&status, p->type, &msg_size);
 MPI_Recv((*mysb)->p+index, msg_size, p->type, status.MPI_SOURCE, 2, p->comm, &status);
